44e1f2c1c24bb64b3bb6dc6afb4284d8d55f5ed4
vinfo <victor.valencia@gmail.com>
2017-07-19 16:01:29 -0500

    feat: checks deviceready

--- platforms/browser/www/index.js
+++ platforms/browser/www/index.js
@@ -1,4 +1,35 @@
 document.addEventListener("deviceready", onDeviceReady, false);
 function onDeviceReady() {
     var ref = cordova.InAppBrowser.open('http://ciaeducon.com/transitoenlinea/index.php', '_self', 'location=no,zoom=no,enableviewportscale=yes');
+    checkConnection();
+	getDeviceProperty();
+}
+function getDeviceProperty() {
+    console.log("getDeviceProperty");
+    var deviceOS = device.platform;  //fetch the device operating system
+    var deviceOSVersion = device.version;  //fetch the device OS version
+    var uuid = device.uuid;
+    localStorage.setItem("OS", deviceOS);
+    localStorage.setItem("UUID", uuid);
+    console.log("Plataforma registrada " + device.platform);
+}
+function checkConnection() {
+    console.log("checkConnection");
+    var state = true;
+    var networkState = navigator.connection.type;
+    var states = {};
+    states[Connection.UNKNOWN] = 'Unknown connection';
+    states[Connection.ETHERNET] = 'Ethernet connection';
+    states[Connection.WIFI] = 'WiFi connection';
+    states[Connection.CELL_2G] = 'Cell 2G connection';
+    states[Connection.CELL_3G] = 'Cell 3G connection';
+    states[Connection.CELL_4G] = 'Cell 4G connection';
+    states[Connection.CELL] = 'Cell generic connection';
+    states[Connection.NONE] = 'No network connection';
+    
+    if (states[networkState] == 'No network connection') {
+            alert('Problemas de conectividad a Internet!');
+            state = false;
+    }
+    return state;
 }
